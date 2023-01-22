6081d00714ca69dcef69d4ccab379b3ad2c096aa
Moiz-I <sky.moizi9@gmail.com>
2023-01-22 19:18:45 +0000

    ganalytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,20 @@
 import "@/styles/globals.css";
 import "./lib/font-awesome/css/all.min.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-C6MRCXCQX7"
+      ></script>
+      <script>
+        window.dataLayer = window.dataLayer || []; function gtag()
+        {dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', 'G-C6MRCXCQX7');
+      </script>
+      <Component {...pageProps} />
+    </>
+  );
 }
